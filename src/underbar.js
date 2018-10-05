4297d0f686466bdc5f885374c7f6aee194c323ee
Rudy <rudydimacali@gmail.com>
2018-10-04 17:00:52 -0700

    Completed underbar part two through _.extend.

--- src/underbar.js
+++ src/underbar.js
@@ -229,12 +229,42 @@
   // Determine whether all of the elements match a truth test.
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
+    if (iterator === undefined) {
+      return collection[collection.length - 1];
+    }
+    return _.reduce(collection, function(passed, testVal) {
+      if (!passed) {
+        return false;
+      } else if (iterator(testVal)) {
+        return true;
+      } else {
+        return false;
+      }
+    }, true);
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    if (collection.length === 0) {
+      return false;
+    }
+    if (iterator === undefined) {
+      return collection[0];
+    }
+    if (_.every(collection, iterator)) {
+      return true;
+    }
+    return _.reduce(collection, function(passed, testVal) {
+      if (passed) {
+        return true;
+      } else if (iterator(testVal)) {
+        return true;
+      } else {
+        return false;
+      }
+    }, false);
   };
 
 
@@ -256,12 +286,27 @@
   //   }, {
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
-  _.extend = function(obj) {
+  _.extend = function(obj, addObj, addObjTwo) {
+    _.each(addObj, function(keys, key) {
+      obj[key] = addObj[key];
+    });
+    _.each(addObjTwo, function(keys, key) {
+      obj[key] = addObjTwo[key];
+    });
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    for (var i = 1; i < arguments.length; i++) {
+      _.each(arguments[i], function(keys,key) {
+        if (obj[key] === undefined) {
+          obj[key] = arguments[i][key];
+        }
+      });
+    }
+    return obj;
   };
 
 
